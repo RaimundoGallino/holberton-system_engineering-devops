78a9b39c060bcd00d0feb8c82aa66a10abf76929
RaimundoGallino <raigallino0@gmail.com>
2021-08-14 00:08:15 -0300

    fixing task 2

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,6 @@
 # execute the comand pkill to terminate (-15) an specific process
 
 exec { 'pkill':
-  command => '/usr/bin/pkill -15 killmenow',
-
+    command => '/usr/bin/pkill -15 killmenow',
 }
  
