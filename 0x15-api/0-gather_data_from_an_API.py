26f5f5961d20daa65e847f076ae126b643e5ddbb
RaimundoGallino <raigallino0@gmail.com>
2021-09-13 16:14:08 -0300

    Adding task 0

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-for a given employee ID, 
+for a given employee ID,
 returns information about his/her TODO list progress.
 """
 
@@ -10,9 +10,9 @@ from sys import argv
 
 if __name__ == "__main__":
     r = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                    .format(argv[1]))
+                     .format(argv[1]))
     u = requests.get('https://jsonplaceholder.typicode.com/users?id={}'
-                    .format(argv[1]))
+                     .format(argv[1]))
 
     name = u.json()[0]['name']
     completed = 0
@@ -25,7 +25,6 @@ if __name__ == "__main__":
             completed += 1
 
     print("Employee {} is done with tasks({}/{}):"
-        .format(name, completed, num_of_tasks))
+          .format(name, completed, num_of_tasks))
     for i in task_list:
         print("\t {}".format(i))
-
