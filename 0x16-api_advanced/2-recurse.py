2863779d570693430cf74d8e0785db831c42bbbc
RaimundoGallino <raigallino0@gmail.com>
2021-09-28 20:22:05 -0300

    uplodaing 2task

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -3,30 +3,6 @@
 
 import requests
 
-
-""" def recurse(subreddit, hot_list=[]):
-    '''Request subs number of given subreddit'''
-    url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
-    req = requests.get(url, headers={'User-agent': 'your bot 0.1'})
-    r = req.json()['data']['children']
-    idx = 0
-    return rec(r, idx, afterId)
-
-def rec(_list, idx, afterId):
-    '''Request subs number of given subreddit'''
-    print(_list[idx]['data']['title'])
-    ret = []
-    idx += 1
-    url = 'https://www.reddit.com/r/{}.json'.format(subreddit, afterId)
-    req = requests.get(url, headers={'User-agent': 'your bot 0.1'})
-    if idx <= len(_list[idx]['data']['title']:
-        ret += _list[idx]['data']['title']
-        afterId = req.json()['data']['after']
-        rec(_list, idx)
-        url += '?after={}'.format(afterId)
-    return ret
- """
-
 def recurse(subreddit, hot_list=[]):
     '''Request subs number of given subreddit'''
     url = 'https://www.reddit.com/r/{}.json'.format(subreddit)
