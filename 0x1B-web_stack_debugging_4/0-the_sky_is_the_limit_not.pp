fcb4a76f9cbf739caf2724089fbeb8ec508e5c4e
RaimundoGallino <69832412+RaimundoGallino@users.noreply.github.com>
2021-10-07 23:17:11 -0300

    Adding task 0

---
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -0,0 +1,8 @@
+# Change the limit of file descriptor nginx can handle
+exec {'Correct Setting':
+    command => "/bin/sed -i 's/15/2048/g' /etc/default/nginx",
+}
+exec { 'Restart Nginx':
+    require => Exec['Correct Setting'],
+    command => '/usr/bin/service nginx restart',
+}
